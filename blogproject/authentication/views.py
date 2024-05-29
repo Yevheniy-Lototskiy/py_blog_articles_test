ec937bfd05e433accc0567a98bba9f3852757388
Eugene <eugene.lot77@gmail.com>
2024-05-29 12:58:49 +0300

    User registration

--- blogproject/authentication/views.py
+++ blogproject/authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, views as auth_views
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "authentication/register.html", {"form": form})
